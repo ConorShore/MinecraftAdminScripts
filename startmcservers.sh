df84b422a7e526ad9b005ac1692169260453ea90
ConorShore <conorshore@gmail.com>
2021-01-08 15:30:59 +0000

    Fixed function names

--- startmcservers.sh
+++ startmcservers.sh
@@ -3,6 +3,6 @@ source commonfuncs.sh
 
 find /home/minecraft0/servers/ -name "server.jar" | awk -F "server" '{print $1 "server" $2}' | xargs -Ihere /usr/bin/screen -dm sh -c 'cd here;java -Xms2G -Xmx2G -jar server.jar nogui '
 
-logtimedate
+logdatetime
 echo "Minecraft Servers Start" >> /home/minecraft0/MinecraftAdminScripts/log
 
--- stopmcservers.sh
+++ stopmcservers.sh
@@ -4,18 +4,18 @@ source commonfuncs.sh
 
 sudo screen -list | grep -v "screen"  | cut -d$'\t' -f 2 | cut -d"." -f 1 | xargs -Ihere sh -c "sudo screen -S here -p 0 -X stuff 'stop^M'"
 
-logtimedate
+logdatetime
 
 echo "Minecraft Servers Shutting Down" >> /home/minecraft0/MinecraftAdminScripts/log
 
 while pids=$(pidof java)
 do
-	logtimedate
+	logdatetime
 	echo "Waiting..." >> /home/minecraft0/MinecraftAdminScripts/log	
 	sleep 1
 done
 
-logtimedate
+logdatetime
 
 echo "Servers Stopped" >> /home/minecraft0/MinecraftAdminScripts/log
 
