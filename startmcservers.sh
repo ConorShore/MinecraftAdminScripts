7d00985677a9c90ed942bd78613dfe365ae3d6e5
ConorShore <conorshore@gmail.com>
2021-01-08 15:45:06 +0000

    Playing to try and get importing conf right

--- startmcservers.sh
+++ startmcservers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source general.conf
 source commonfuncs.sh
 
 find "$(serversdir)" -name "server.jar" | awk -F "server" '{print $1 "server" $2}' | xargs -Ihere /usr/bin/screen -dm sh -c 'cd here;java -Xms2G -Xmx2G -jar server.jar nogui '
