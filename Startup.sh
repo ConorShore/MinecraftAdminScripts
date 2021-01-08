267fa26c8f3a834781298a45fc81b723fcea1ebe
ConorShore <conorshore@gmail.com>
2021-01-08 17:36:54 +0000

    fixing startup.sh

--- Startup.sh
+++ Startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /home/minecraft0/MinecraftAdminScripts/includes.conf;.$scriptsdir startmcservers.sh
-
-
-.$scriptsdir upnpset.sh
+source /home/minecraft0/MinecraftAdminScripts/includes.conf
+$scriptsdir/startmcservers.sh
+$scriptsdir/upnpset.sh
