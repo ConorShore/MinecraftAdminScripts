e0ea6dc603426e3e5d5c625443f03a36737be5e1
ConorShore <conorshore@gmail.com>
2021-01-08 15:52:48 +0000

    Fixed clearadminlogs.sh to use the conf file

--- clearadminlogs.sh
+++ clearadminlogs.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-function logtime {
+source commonfuncs.sh
+rm $logdir
 
-	daten=$(date +'%m/%d/%Y')
-	timen=$(date +"%T")
-	echo -n $daten >> /home/minecraft0/MinecraftAdminScripts/log
-	echo -n " " >> /home/minecraft0/MinecraftAdminScripts/log
-	echo -n $timen >> /home/minecraft0/MinecraftAdminScripts/log
-	echo -n " " >> /home/minecraft0/MinecraftAdminScripts/log
-
-}
-rm /home/minecraft0/MinecraftAdminScripts/log
-
-echo " " > /home/minecraft0/MinecraftAdminScripts/log
-logtime
-echo "Log file cleared" >> /home/minecraft0/MinecraftAdminScripts/log
+echo " " > $logdir
+logdatetime
+echo "Log file cleared" >> $logdir
 
