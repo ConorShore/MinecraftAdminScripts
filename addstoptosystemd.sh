d332927526414ef1e8cf92403017f3091b44c275
ConorShore <conorshore@gmail.com>
2021-01-08 16:09:15 +0000

    Started work on systemd service generator

--- addstoptosystemd.sh
+++ addstoptosystemd.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 source commonfuncs.sh
+
+
+printf "\
+[Unit]\n
+Description=Stops minecraft servers on shutdown\n\
+Before=shutdown.target reboot.target halt.target\n\
+\n\
+[Service]\n\
+User=root\n\
+Type=oneshot\n\
+RemainAfterExit=true\n\
+ExecStop=" "$scripts" "/stopmcservers.sh\n\
+\n\
+[Install]\n\
+WantedBy=multi-user.target\n\
+"
