c37035fa134bd163b0377c688caca9a60fce3038
ConorShore <conorshore@gmail.com>
2021-01-08 15:43:52 +0000

    Added some quotes to try fix things

--- startmcservers.sh
+++ startmcservers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source commonfuncs.sh
 
-find $(serversdir) -name "server.jar" | awk -F "server" '{print $1 "server" $2}' | xargs -Ihere /usr/bin/screen -dm sh -c 'cd here;java -Xms2G -Xmx2G -jar server.jar nogui '
+find "$(serversdir)" -name "server.jar" | awk -F "server" '{print $1 "server" $2}' | xargs -Ihere /usr/bin/screen -dm sh -c 'cd here;java -Xms2G -Xmx2G -jar server.jar nogui '
 
 logdatetime
-echo "Minecraft Servers Start" >> $(logdir)
+echo "Minecraft Servers Start" >> "$(logdir)"
 
